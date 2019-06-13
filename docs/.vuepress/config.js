bb42829bab3d2d9b413305d70db203fdfa6e6232
codexu <461229187@qq.com>
2019-06-13 17:57:26 +0800

    docs: ✏️ add nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,18 @@
 module.exports = {
   title: 'X-BUILD 中文文档',
-  description: 'X-BUILD 是面向小型项目的脚手架工具，通过终端命令最快在几秒钟初始化项目目录，解决开发中的自动化、性能、模块化、规范、自适应布局等问题，辅助你生成更优质的代码。',
+  description: 'X-BUILD是面向小型项目的脚手架工具，通过终端命令最快在几秒钟初始化项目目录，解决开发中的自动化、性能、模块化、规范、自适应布局等问题，辅助你生成更优质的代码。',
   themeConfig: {
+    repo: 'codexu/x-build',
+    repoLabel: '查看源码',
+    docsRepo: 'codexu/codexu.github.io',
+    docsDir: 'docs',
+    docsBranch: 'docs',
+    editLinks: true,
+    editLinkText: '帮助我改善此页面！',
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Github', link: 'https://github.com/codexu/x-build' },
+      { text: '快速上手', link: '/' },
+      { text: '指南', link: '/概念' },
+      { text: '工具', link: '/utils/快速上手' },
     ],
     sidebar: [
       {
@@ -12,22 +20,22 @@ module.exports = {
         collapsable: false,
         children: [
           ['/', '概述'],
-          ['/安装命令行工具', '安装命令行工具 (CLI)'],
+          ['/命令行工具', '命令行工具 (CLI)'],
           ['/快速开始', '快速开始'],
+          ['/创建项目', '创建项目'],
         ]
       },
       {
-        title: '更多功能',
+        title: '指南',
         collapsable: false,
         children: [
-          ['/创建项目', '创建项目'],
           ['/概念', '概念'],
           ['/配置', '配置'],
           ['/规范', '规范'],
         ]
       },
       {
-        title: '其他工具',
+        title: '工具',
         collapsable: false,
         children: [
           ['/utils/快速上手', '快速上手'],
@@ -38,6 +46,6 @@ module.exports = {
           ['/utils/x-music', 'X-MUSIC'],
         ]
       },
-    ]
-  }
+    ],
+  },
 }
